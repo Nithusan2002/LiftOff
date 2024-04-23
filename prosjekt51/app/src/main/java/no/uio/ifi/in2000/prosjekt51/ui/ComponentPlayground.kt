eaec3524310e37afa089f297dbe86980a2ba1dbb
Dusan Dobrik <d.s.dobrik@gmail.com>
2024-04-23 09:07:04 +0200

    Add relay in gradle, add initial files for all screens and some components. Still need to figure out how to theme components, but they should be functional

---
+++ prosjekt51/app/src/main/java/no/uio/ifi/in2000/prosjekt51/ui/ComponentPlayground.kt
@@ -0,0 +1,10 @@
+package no.uio.ifi.in2000.prosjekt51.ui
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Preview
+@Composable
+fun favoritePreview() {
+
+}
